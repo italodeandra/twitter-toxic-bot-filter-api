7aae2e174d142a4c9b098264f15605ecae2f4294
Ítalo Andrade <italodeandra@gmail.com>
2020-04-13 17:18:06 +0200

    handle twitter error 415

--- test/loadtest.development.ts
+++ test/loadtest.development.ts
@@ -1,18 +1,18 @@
-import loadtest from 'loadtest'
-
-console.log('[Test] Starting load test...')
-
-const options = {
-    url: 'http://localhost:3001/',
-    maxRequests: 1000,
-    // concurrency: 100,
-    maxSeconds: 10,
-    // requestsPerSecond: 100
-}
-
-loadtest.loadTest(options, function (error: Error, result: any) {
-    if (error) {
-        return console.error('[Test] Got an error', error)
-    }
-    console.log('[Test] Tests run successfully', result)
-})
+// import loadtest from 'loadtest'
+//
+// console.log('[Test] Starting load test...')
+//
+// const options = {
+//     url: 'http://localhost:3001/',
+//     maxRequests: 1000,
+//     // concurrency: 100,
+//     maxSeconds: 10,
+//     // requestsPerSecond: 100
+// }
+//
+// loadtest.loadTest(options, function (error: Error, result: any) {
+//     if (error) {
+//         return console.error('[Test] Got an error', error)
+//     }
+//     console.log('[Test] Tests run successfully', result)
+// })
