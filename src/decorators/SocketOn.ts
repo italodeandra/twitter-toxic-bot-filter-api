35af3ed9dafe000c8595361241d144a0bddaeb1b
Ítalo Andrade <italodeandra@gmail.com>
2020-04-14 23:45:38 +0200

    checking for auth inside the decorator to reuse code

--- src/decorators/SocketOn.ts
+++ src/decorators/SocketOn.ts
@@ -1,9 +1,21 @@
 import socket from '../socket'
+import { Socket } from 'socket.io'
+import { User } from '../api/User/UserEntity'
 
 export function SocketOn(event: string, auth: boolean = false) {
     return function (target: any, propertyKey: string) {
         socket.events.push({
-            name: event, handler: target[propertyKey], auth
+            name: event,
+            handler(socket: Socket, ...props) {
+                if (auth) {
+                    const user: User = (socket as any).user
+                    if (!user) {
+                        return null
+                    }
+                }
+                target[propertyKey](socket, props)
+            },
+            auth
         })
     }
 }
