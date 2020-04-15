e27224db595de2a85ca315ef12d89d2b6977022e
Ítalo Andrade <italodeandra@gmail.com>
2020-04-15 08:50:36 +0200

    log stringified but with indent

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,4 +1,5 @@
 import { User } from '../api/User/UserEntity'
+import config from '../config'
 
 type Log = {
     type?: 'info'
@@ -77,9 +78,9 @@ export function log(type: 'info' | 'error', data: Log): void {
     data.type = type
     let consoleType: 'log' | 'info' | 'error' = type
     if (type === 'info') consoleType = 'log'
-    // if (config.env === 'development') {
-    console[consoleType](data)
-    // } else {
-    //     console[consoleType](JSON.stringify(data))
-    // }
+    if (config.env === 'development') {
+        console[consoleType](data)
+    } else {
+        console[consoleType](JSON.stringify(data), null, 2)
+    }
 }
