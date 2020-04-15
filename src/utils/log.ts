5371d8242a0900d3b2483185496f8498722b8501
Ítalo Andrade <italodeandra@gmail.com>
2020-04-15 14:36:39 +0200

    log with winston simple format

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -5,7 +5,7 @@ const logger = winston.createLogger({
 })
 
 logger.add(new winston.transports.Console({
-    format: winston.format.json()
+    format: winston.format.simple()
 }))
 
 export default function log(entry: LogEntry): Logger {
