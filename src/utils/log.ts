3a2ad60fc4d08991a7cac0d990dbff6b13411383
Ítalo Andrade <italodeandra@gmail.com>
2020-04-15 14:36:06 +0200

    always log with winston json format

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,15 +1,12 @@
-import config from '../config'
 import winston, { LogEntry, Logger } from 'winston'
 
 const logger = winston.createLogger({
     format: winston.format.json(),
 })
 
-if (config.env !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple()
-    }))
-}
+logger.add(new winston.transports.Console({
+    format: winston.format.json()
+}))
 
 export default function log(entry: LogEntry): Logger {
     return logger.log(entry)
